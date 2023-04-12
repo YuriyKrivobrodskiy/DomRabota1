e93cc738df6c021b981856c8ecf361715b2c4390
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-13 00:50:55 +0300

    zadacha6

---
+++ zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿// программа, которая принимает на вход число и выдает, является ли оно четным
+System.Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+
+if (a % 2 == 0)
+   {
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+} 
