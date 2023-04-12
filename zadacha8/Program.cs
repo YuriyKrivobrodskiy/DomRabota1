0f6f4f280d477ab9efe33fbaa0d70449b273c2c7
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-13 01:09:22 +0300

    zadacha8

---
+++ zadacha8/Program.cs
@@ -0,0 +1,12 @@
+﻿// программа, которая на вход принимает число (N), а на выходе показывает все четные числа от 1 до N
+System.Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+
+int i = 1;
+
+while (i <= N) {
+if (i % 2 == 0) {
+    System.Console.Write(i + " ");
+}
+i++;
+}
