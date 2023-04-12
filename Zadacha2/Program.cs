0d9912d0acc10ccbc704987e4c7688290d7103e5
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-12 23:01:03 +0300

    zadacha2

---
+++ Zadacha2/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее
+
+System.Console.WriteLine("Введите первое число ");
+int a = int.Parse(Console.ReadLine());
+
+System.Console.WriteLine("Введите второе число ");
+int b = int.Parse(Console.ReadLine());
+
+if ((a) == (b)) {
+    System.Console.Write(a);
+    System.Console.Write(" = ");
+    System.Console.WriteLine(b);
+}
+else {
+    if ( a > b ) {
+        System.Console.Write("Max = ");
+        System.Console.WriteLine(a);
+
+        System.Console.Write("Min = ");
+        System.Console.Write(b);
+    }
+    else {
+        System.Console.Write("Max = ");
+        System.Console.WriteLine(b);
+
+        System.Console.Write("Min = ");
+        System.Console.Write(a);
+    }
+}
