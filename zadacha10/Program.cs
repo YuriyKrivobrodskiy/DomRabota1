9b69f00bbb384090a3e1dd95fdbccc947e88719a
kaktus <yuriykrivobrodskiy@yandex.ru>
2023-04-16 23:25:37 +0300

    zadacha10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,16 @@
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+ System.Console.WriteLine("Введите трёхзначное число: ");
+ int number = int.Parse(Console.ReadLine());
+ int i;
+
+ if (number > 99 && number < 1000)
+ {
+    i = number % 100;
+    i = i / 10;
+    System.Console.WriteLine(i);
+ }
+ else
+ {
+    System.Console.WriteLine("Вы ввели не трёхзначное число.");
+ }
